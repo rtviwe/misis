66621e6fccf84bfd53b816d689941ee9c15fe034
Igor <rtviwe@gmail.com>
2018-12-11 20:22:13 +0300

    Fix complex

--- spiridonov_i_g/labs/complex/complex.cpp
+++ spiridonov_i_g/labs/complex/complex.cpp
@@ -8,50 +8,50 @@ Complex::Complex(const double real) : Complex(real, 0.0) {}
 Complex::Complex(const double real,
     const double imaginary) : re(real), im(imaginary) {}
 
-bool Complex::operator==(const Complex & rhs) const {
+bool Complex::operator==(const Complex& rhs) const {
     return (re == rhs.re) && (im == rhs.im);
 }
 
-bool Complex::operator!=(const Complex & rhs) const {
+bool Complex::operator!=(const Complex& rhs) const {
     return !operator==(rhs);
 }
 
-Complex & Complex::operator+=(const double rhs) {
+Complex& Complex::operator+=(const double rhs) {
     return operator+=(Complex(rhs));
 }
 
-Complex & Complex::operator-=(const double rhs) {
+Complex& Complex::operator-=(const double rhs) {
     return operator-=(Complex(rhs));
 }
 
-Complex & Complex::operator*=(const double rhs) {
+Complex& Complex::operator*=(const double rhs) {
     return operator*=(Complex(rhs));
 }
 
-Complex & Complex::operator/=(const double rhs) {
+Complex& Complex::operator/=(const double rhs) {
     return operator/=(Complex(rhs));
 }
 
-Complex & Complex::operator+=(const Complex & rhs) {
+Complex& Complex::operator+=(const Complex& rhs) {
     re += rhs.re;
     im += rhs.im;
     return *this;
 }
 
-Complex & Complex::operator-=(const Complex & rhs) {
+Complex& Complex::operator-=(const Complex& rhs) {
     re -= rhs.re;
     im -= rhs.im;
     return *this;
 }
 
-Complex & Complex::operator*=(const Complex & rhs) {
+Complex& Complex::operator*=(const Complex& rhs) {
     int tempRe = re;
     re = (re * rhs.re) - (im * rhs.im);
     im = (tempRe * rhs.im) + (im * rhs.re);
     return *this;
 }
 
-Complex & Complex::operator/=(const Complex & rhs) {
+Complex& Complex::operator/=(const Complex& rhs) {
     if (((rhs.re * rhs.re) + (rhs.im * rhs.im)) == 0) {
         throw std::exception("Divide by zero exception");
     }
@@ -64,12 +64,12 @@ Complex & Complex::operator/=(const Complex & rhs) {
     return *this;
 }
 
-std::ostream & Complex::writeTo(std::ostream & ostrm) const {
+std::ostream& Complex::writeTo(std::ostream& ostrm) const {
     ostrm << leftBrace << re << separator << im << rightBrace;
     return ostrm;
 }
 
-std::istream & Complex::readFrom(std::istream & istrm) {
+std::istream& Complex::readFrom(std::istream& istrm) {
     char leftBrace(0);
     double real(0.0);
     char comma(0);
@@ -89,38 +89,33 @@ std::istream & Complex::readFrom(std::istream & istrm) {
     return istrm;
 }
 
-std::ostream & operator<<(std::ostream & ostrm,
-    const Complex & rhs) {
+std::ostream& operator<<(std::ostream& ostrm, const Complex & rhs) {
     return rhs.writeTo(ostrm);
 }
 
-std::istream & operator>>(std::istream & istrm, Complex & rhs) {
+std::istream& operator>>(std::istream& istrm, Complex& rhs) {
     return rhs.readFrom(istrm);
 }
 
-Complex operator+(const Complex & lhs,
-    const Complex & rhs) {
+Complex operator+(const Complex& lhs, const Complex& rhs) {
     Complex sum(lhs);
     sum += rhs;
     return sum;
 }
 
-Complex operator-(const Complex & lhs,
-    const Complex & rhs) {
+Complex operator-(const Complex& lhs, const Complex& rhs) {
     Complex sub(lhs);
     sub -= rhs;
     return sub;
 }
 
-Complex operator*(const Complex & lhs,
-    const Complex & rhs) {
+Complex operator*(const Complex& lhs, const Complex& rhs) {
     Complex mul(lhs);
     mul *= rhs;
     return mul;
 }
 
-Complex operator/(const Complex & lhs,
-    const Complex & rhs) {
+Complex operator/(const Complex& lhs, const Complex& rhs) {
     Complex div(lhs);
     div /= rhs;
     return div;
--- spiridonov_i_g/labs/complex/complex.hpp
+++ spiridonov_i_g/labs/complex/complex.hpp
@@ -14,23 +14,23 @@ struct Complex {
 
     ~Complex() = default;
 
-    bool operator==(const Complex & rhs) const;
-    bool operator!=(const Complex & rhs) const;
+    bool operator==(const Complex& rhs) const;
+    bool operator!=(const Complex& rhs) const;
 
-    Complex & operator+=(const Complex & rhs);
-    Complex & operator+=(const double rhs);
+    Complex& operator+=(const Complex& rhs);
+    Complex& operator+=(const double rhs);
 
-    Complex & operator-=(const Complex & rhs);
-    Complex & operator-=(const double rhs);
+    Complex& operator-=(const Complex& rhs);
+    Complex& operator-=(const double rhs);
 
-    Complex & operator*=(const Complex & rhs);
-    Complex & operator*=(const double rhs);
+    Complex& operator*=(const Complex& rhs);
+    Complex& operator*=(const double rhs);
 
-    Complex & operator/=(const Complex & rhs);
-    Complex & operator/=(const double rhs);
+    Complex& operator/=(const Complex& rhs);
+    Complex& operator/=(const double rhs);
 
-    std::ostream & writeTo(std::ostream & ostrm) const;
-    std::istream & readFrom(std::istream & istrim);
+    std::ostream& writeTo(std::ostream& ostrm) const;
+    std::istream& readFrom(std::istream& istrim);
 
     double re { 0.0 };
     double im { 0.0 };
@@ -40,17 +40,12 @@ struct Complex {
     static const char rightBrace { '}' };
 };
 
-std::ostream & operator<<(std::ostream & ostrm,
-    const Complex & rhs);
-std::istream & operator>>(std::istream & istrm, Complex & rhs);
+std::ostream & operator<<(std::ostream& ostrm, const Complex& rhs);
+std::istream & operator>>(std::istream& istrm, Complex& rhs);
 
-Complex operator+(const Complex & lhs,
-    const Complex & rhs);
-Complex operator-(const Complex & lhs,
-    const Complex & rhs);
-Complex operator*(const Complex & lhs,
-    const Complex & rhs);
-Complex operator/(const Complex & lhs,
-    const Complex & rhs);
+Complex operator+(const Complex& lhs, const Complex& rhs);
+Complex operator-(const Complex& lhs, const Complex& rhs);
+Complex operator*(const Complex& lhs, const Complex& rhs);
+Complex operator/(const Complex& lhs, const Complex& rhs);
 
 #endif
